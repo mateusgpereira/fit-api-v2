141af730abbda8311cf5801be738f4eaa0d86485
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2024-05-28 22:40:13 +0200

    feat(base): commit init

---
+++ src/main/java/io/tora/fitapiv2/FitApiV2Application.java
@@ -0,0 +1,13 @@
+package io.tora.fitapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitApiV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FitApiV2Application.class, args);
+  }
+
+}
---
+++ src/test/java/io/tora/fitapiv2/FitApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tora.fitapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitApiV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
